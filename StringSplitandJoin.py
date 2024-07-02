fcc98fe65163af6d83ca18c6dccc749c5ffa8a94
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-07-02 11:32:09 +0530

    Add files via upload

---
+++ StringSplitandJoin.py
@@ -0,0 +1,15 @@
+"""
+Title     : String Split and Join
+Subdomain : Strings
+Domain    : Python
+"""
+
+
+def split_and_join(sentence):
+    return "-".join(sentence.split())
+
+
+if __name__ == "__main__":
+    line = input()
+    result = split_and_join(line)
+    print(result)
